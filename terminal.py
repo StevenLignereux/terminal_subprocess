96ecdac9b74e0f2d113d6973d684253c452fef18
Steven Lignereux <slignere@gmail.com>
2024-10-17 08:14:53 +0200

    📦 NEW: Executer des commandes depuis python

--- terminal.py
+++ terminal.py
@@ -1,1 +1,6 @@
 # CREER UN TERMINAL AVEC PYTHON
+import subprocess
+
+# Popen : ancienne interface
+# run : executer la commande et attendre le résultat
+subprocess.run("dir", shell=True)  # ls sur mac
