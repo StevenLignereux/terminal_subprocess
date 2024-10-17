0306c272a129b0f51fbd12c6a67b36855254c47c
Steven Lignereux <slignere@gmail.com>
2024-10-17 08:24:11 +0200

    📦 NEW: Récupérer la sortie

--- terminal.py
+++ terminal.py
@@ -3,4 +3,9 @@ import subprocess
 
 # Popen : ancienne interface
 # run : executer la commande et attendre le résultat
-subprocess.run("dir", shell=True)  # ls sur mac
+resultat = subprocess.run(
+    "dir", shell=True, capture_output=True, universal_newlines=True
+)  # ls sur mac
+
+print(resultat.stdout)
+print(resultat.stderr)
