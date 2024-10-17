fd1bed533c4ccf9e1c706d1ff01d97d8ed922cb3
Steven Lignereux <slignere@gmail.com>
2024-10-17 08:38:14 +0200

    📦 NEW: Demander des commandes à l'utilisateur

--- terminal.py
+++ terminal.py
@@ -3,9 +3,17 @@ import subprocess
 
 # Popen : ancienne interface
 # run : executer la commande et attendre le résultat
-resultat = subprocess.run(
-    "dir", shell=True, capture_output=True, universal_newlines=True
-)  # ls sur mac
 
-print(resultat.stdout)
-print(resultat.stderr)
+while True:
+    commande = input("Tapez quelque chose (ou 'exit' pour quitter) : ")
+
+    if commande == "exit":
+        print("Au revoir!")
+        break
+
+    resultat = subprocess.run(
+        commande, shell=True, capture_output=True, universal_newlines=True
+    )  # ls sur mac
+
+    print(resultat.stdout)
+    print(resultat.stderr)
