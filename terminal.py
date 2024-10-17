d2060339144f66045ad8231b29f02cd257966234
Steven Lignereux <slignere@gmail.com>
2024-10-17 09:10:32 +0200

    📦 NEW: changer de répértoire et répertoire invalide

--- terminal.py
+++ terminal.py
@@ -1,19 +1,32 @@
 # CREER UN TERMINAL AVEC PYTHON
+import os
 import subprocess
 
 # Popen : ancienne interface
 # run : executer la commande et attendre le résultat
 
+# os.getcwd()
+# os.chdir(...)
+
 while True:
-    commande = input("Tapez quelque chose (ou 'exit' pour quitter) : ")
+    commande = input(os.getcwd() + " > ")
 
     if commande == "exit":
         print("Au revoir!")
         break
 
-    resultat = subprocess.run(
-        commande, shell=True, capture_output=True, universal_newlines=True
-    )  # ls sur mac
+    commande_split = commande.split(" ")
 
-    print(resultat.stdout)
-    print(resultat.stderr)
+    if len(commande_split) == 2 and commande_split[0] == "cd":
+        try:
+            os.chdir(commande_split[1])
+        except FileNotFoundError:
+            print("Erreur ! Ce répertoire n'existe pas")
+
+    else:
+        resultat = subprocess.run(
+            commande, shell=True, capture_output=True, universal_newlines=True
+        )  # ls sur mac
+
+        print(resultat.stdout)
+        print(resultat.stderr)
